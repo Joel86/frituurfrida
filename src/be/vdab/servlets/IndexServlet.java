9e70f4fc2ecf2cc82b84b0ec49ad93c9490d60cc
Joel Vanherle <joel.vanherle@hotmail.com>
2017-09-05 10:54:22 +0200

    Changed telefoonHelpdesk init param to servlet context init param

--- src/be/vdab/servlets/IndexServlet.java
+++ src/be/vdab/servlets/IndexServlet.java
@@ -21,7 +21,7 @@ public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static final String VIEW = "/WEB-INF/JSP/index.jsp";
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setAttribute("telefoonHelpdesk", this.getInitParameter("telefoonHelpdesk"));
+		request.setAttribute("telefoonHelpdesk", this.getServletContext().getInitParameter("telefoonHelpdesk"));
 		LocalDate vandaag = LocalDate.now();
 		DayOfWeek weekdag = vandaag.getDayOfWeek();
 		request.setAttribute("openGesloten", weekdag == DayOfWeek.MONDAY || weekdag == DayOfWeek.THURSDAY ?
